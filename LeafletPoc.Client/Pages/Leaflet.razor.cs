4eb64cb09166c6b42cfd7401271fc6a43e7bf420
Oceane DUQUENNE <oceane.duquenne@capgemini.com>
2024-03-13 12:11:33 +0100

    Ajoutez des fichiers projet.

---
+++ LeafletPoc.Client/Pages/Leaflet.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace LeafletPoc.Client.Pages
+{
+    public partial class Leaflet : ComponentBase, IAsyncDisposable
+    {
+        [Inject] IJSRuntime JSRuntime { get; set; } = default!;
+        private IJSObjectReference? module;
+        private string? result;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                result = await JSRuntime.InvokeAsync<string>("loadMap");
+            }
+        }
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            if (module is not null)
+            {
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ LeafletPoc.Client/Pages/Leaflet.razor.js
@@ -0,0 +1,5 @@
+﻿function loadMap(raw) {
+    let map = L.map('map').setView({ lon: 26.097133, lat: 44.446165 }, 16);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+    return "";
+}

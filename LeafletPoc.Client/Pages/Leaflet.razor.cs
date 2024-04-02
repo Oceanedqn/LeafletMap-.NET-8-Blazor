5229bf5c1ca24ba804f379d3ca579324fb089cfe
Oceane DUQUENNE <oceane.duquenne@capgemini.com>
2024-04-02 17:02:06 +0200

    Ajout des clusters avec "markercluster", customisation des icônes concernant les points, customisation de l'icone du cluster

--- LeafletPoc.Client/Pages/Leaflet.razor.cs
+++ LeafletPoc.Client/Pages/Leaflet.razor.cs
@@ -1,7 +1,6 @@
 ﻿using LeafletPoc.Client.Dialogs;
 using LeafletPoc.Client.Models;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using MudBlazor;
 
@@ -12,19 +11,15 @@ namespace LeafletPoc.Client.Pages
         [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
         [Inject] public IDialogService DialogService { get; set; } = default!;
 
-        private string? _result;
-        private string? _locationMessage;
         private Location _location = new Location { Latitude = 0, Longitude = 0 };
-
         private List<Location> _locations = new List<Location>();
-
-
 
 
         protected override void OnInitialized()
         {
             GetLocations();
         }
+
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -34,7 +29,7 @@ namespace LeafletPoc.Client.Pages
 
                 var dotNetReference = DotNetObjectReference.Create(this);
 
-                _result = await JSRuntime.InvokeAsync<string>("loadMap", _location.Latitude, _location.Longitude);
+                await JSRuntime.InvokeVoidAsync("loadMap", _location.Latitude, _location.Longitude);
                 await JSRuntime.InvokeVoidAsync("displayMarkers", _locations, dotNetReference);
             }
         }
@@ -48,7 +43,7 @@ namespace LeafletPoc.Client.Pages
             _locations = new List<Location>()
             {
                 new Location {Id = 0, Name = "les pissenlits", Latitude = 48.688347, Longitude = 6.1819333, Type = LocationType.Restaurant },
-                new Location {Id = 1, Name = "Vins et Tartines", Latitude = 48.688347, Longitude = 6.1819333, Type = LocationType.Poissonerie},
+                new Location {Id = 1, Name = "Vins et Tartines", Latitude = 48.678347, Longitude = 6.1819333, Type = LocationType.Poissonerie},
                 new Location {Id = 2, Name = "Café du commerce", Latitude = 48.6931448, Longitude = 6.1825616, Type = LocationType.Bar},
                 new Location {Id = 3, Name = "La table de stan", Latitude = 48.6937738, Longitude= 6.1809019, Type = LocationType.Restaurant},
             };
@@ -61,16 +56,9 @@ namespace LeafletPoc.Client.Pages
         /// <returns>Return its current location</returns>
         private async Task RequestLocationPermissionAsync()
         {
-            try
-            {
-                _location = await JSRuntime.InvokeAsync<Location>("requestLocationPermission");
-                _locationMessage = $"Latitude: {_location.Latitude}, Longitude: {_location.Longitude}";
-            }
-            catch (Exception ex)
-            {
-                _locationMessage = $"Erreur lors de la récupération de la position : {ex.Message}";
-            }
+            _location = await JSRuntime.InvokeAsync<Location>("requestLocationPermission");
         }
+
 
         /// <summary>
         /// Method invoked from the js file. Opens the dialog
--- LeafletPoc.Client/Pages/Leaflet.razor.js
+++ LeafletPoc.Client/Pages/Leaflet.razor.js
@@ -1,4 +1,5 @@
-﻿let map;
+﻿/* eslint-disable indent */
+let map;
 
 window.requestLocationPermission = function () {
     return new Promise((resolve, reject) => {
@@ -16,7 +17,7 @@ window.requestLocationPermission = function () {
                         console.error("L'information de localisation n'est pas disponible.");
                         break;
                     case error.TIMEOUT:
-                        console.error("La demande de géolocalisation a expiré.");
+                        console.error('La demande de géolocalisation a expiré.');
                         break;
                     case error.UNKNOWN_ERROR:
                         console.error("Une erreur inconnue s'est produite.");
@@ -30,26 +31,49 @@ window.requestLocationPermission = function () {
     });
 };
 
-
 // Loads and displays the map. Also displays a marker at the user's position.
-function loadMap(latitude, longitude) {
+function loadMap (latitude, longitude) {
     map = L.map('map', { center: [latitude, longitude], zoom: 16 });
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
 
     L.marker([latitude, longitude]).addTo(map)
-        .bindPopup("You are here")
+        .bindPopup('You are here')
         .openPopup();
-    return "";
 }
-
 
 // Displays the various markers and defines the dialog opening function.
 function displayMarkers(locations, dotNetObject) {
-    locations.forEach(location => {
-        L.marker([location.latitude, location.longitude]).addTo(map)
-            .on('click', function (e) {
-                dotNetObject.invokeMethodAsync('OpenDetailDialog', location);
-            });
+    const restaurantIcon = L.icon({
+        iconUrl: 'img/restaurantIcon.png',
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
-}
+    const pubIcon = L.icon({
+        iconUrl: 'img/pubIcon.png',
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    const markers = new L.MarkerClusterGroup({
+        iconCreateFunction: function (cluster) {
+            let markers = cluster.getAllChildMarkers();
+            let html = '<div class="circle">' + markers.length + '</div>';
+            return L.divIcon({ html: html, className: 'mycluster', iconSize: L.point(32, 32) });
+        }
+    });
+    let typeIcon;
 
+    locations.forEach(location => {
+        if (location.type == 0) {
+            typeIcon = restaurantIcon;
+        } else {
+            typeIcon = pubIcon;
+        }
+        markers.addLayer(L.marker([location.latitude, location.longitude], { icon: typeIcon }).addTo(map)
+                .on('click', function (e) {
+                    dotNetObject.invokeMethodAsync('OpenDetailDialog', location)
+                }));
+    });
+
+    map.addLayer(markers);
+}
